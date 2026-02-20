48ffec437e4064b7b2c41f605bb307b4768248f3
agampa263 <133222558+agampa263@users.noreply.github.com>
2026-02-20 15:03:46 +0530

    Update UsbDevice_L2Test.cpp

--- Tests/L2Tests/tests/UsbDevice_L2Test.cpp
+++ Tests/L2Tests/tests/UsbDevice_L2Test.cpp
@@ -28,7 +28,7 @@
 
 #define TEST_LOG(x, ...) fprintf(stderr, "\033[1;32m[%s:%d](%s)<PID:%d><TID:%d>" x "\n\033[0m", __FILE__, __LINE__, __FUNCTION__, getpid(), gettid(), ##__VA_ARGS__); fflush(stderr);
 
-#define JSON_TIMEOUT   (1000)
+#define JSON_TIMEOUT   (8000)
 #define USBDEVICE_CALLSIGN  _T("org.rdk.UsbDevice")
 #define USBDEVICEL2TEST_CALLSIGN _T("L2tests.1")
 
