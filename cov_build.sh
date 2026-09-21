8f1b8fc87b8eb627f985535b91916e457627914f
balav08 <54432605+balav08@users.noreply.github.com>
2026-09-21 23:51:22 +0530

    Update cov_build.sh

--- cov_build.sh
+++ cov_build.sh
@@ -48,6 +48,7 @@ cmake -G Ninja -S "$GITHUB_WORKSPACE" -B build/entservices-usbdevice \
 -I ${GITHUB_WORKSPACE}/entservices-testframework/Tests/headers/ccec/drivers \
 -I ${GITHUB_WORKSPACE}/entservices-testframework/Tests/headers/network \
 -I ${GITHUB_WORKSPACE}/entservices-testframework/Tests/headers/Dobby \
+-I ${GITHUB_WORKSPACE}/entservices-testframework/Tests/headers/libusb \
 -I ${GITHUB_WORKSPACE}/entservices-testframework/Tests/headers/Dobby/Public/Dobby \
 -I ${GITHUB_WORKSPACE}/entservices-testframework/Tests/headers/Dobby/IpcService \
 -I ${GITHUB_WORKSPACE}/entservices-testframework/Tests/mocks \
@@ -63,7 +64,7 @@ cmake -G Ninja -S "$GITHUB_WORKSPACE" -B build/entservices-usbdevice \
 -include ${GITHUB_WORKSPACE}/entservices-testframework/Tests/mocks/pkg.h \
 -include ${GITHUB_WORKSPACE}/entservices-testframework/Tests/mocks/maintenanceMGR.h \
 -include ${GITHUB_WORKSPACE}/entservices-testframework/Tests/mocks/secure_wrappermock.h \
--include ${GITHUB_WORKSPACE}/entservices-testframework/Tests/mocks/libusb/libusb_compat.h \
+-include ${GITHUB_WORKSPACE}/entservices-testframework/Tests/mocks/libusb/libusb.h \
 -include ${GITHUB_WORKSPACE}/entservices-testframework/Tests/mocks/Dobby.h \
 -include ${GITHUB_WORKSPACE}/entservices-testframework/Tests/headers/Dobby/DobbyProtocol.h \
 -include ${GITHUB_WORKSPACE}/entservices-testframework/Tests/headers/Dobby/DobbyProxy.h \
